c9abf7037fe4e58e19462bd9061a736055f5f1b3
RoseGabriely <rosegabriely14@gmail.com>
2021-09-10 17:30:08 -0400

    used useEffect to pull data from the API, and passed it down to the character component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState} from 'react';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,23 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characterData, setCharacterData] = useState(null)
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people`)
+    .then(res => {
+      setCharacterData(res.data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+  
+  console.log('test', characterData)
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character characterData={characterData}/>
     </div>
   );
 }
